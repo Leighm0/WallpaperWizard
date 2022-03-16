52e4bb80466337c7f64f3c0d088075bcd04f81b7
Leighm0 <69341431+Leighm0@users.noreply.github.com>
2022-03-16 10:08:24 +0800

    version 5

--- driver.lua
+++ driver.lua
@@ -1,82 +1,69 @@
----------------
--- Globals
----------------
+-------------
+-- Globals --
+-------------
 do
 	EC = {}
 	OPC = {}
 end
 
------------------------------------------------------------------------------
---Function Name : SendToDevices
---Parameters    : tList(table), strCommand(string), tParams(table)
---Description   : Function called to send wallpaper change to Identity agent.
------------------------------------------------------------------------------
-function SendToDevices(tList, strCommand, strWallpaper)
-	tList = trim(tList) .. ","
-	local tParams = {}
-	local agent = "control4_agent_identity.c4i"
-	local devices = C4:GetDevicesByC4iName(agent)
-	local proxyId = ""
-	if (devices ~= nil) then
-		for k,v in pairs(devices) do
-			if (v == "Identity") then proxyId = k end
-		end
+----------------------------------------------------------------------------
+--Function Name : OnDriverInit
+--Description   : Function invoked when a driver is loaded or being updated.
+----------------------------------------------------------------------------
+function OnDriverInit()
+	C4:UpdateProperty("Driver Name", C4:GetDriverConfigInfo("name"))
+	C4:UpdateProperty("Driver Version", C4:GetDriverConfigInfo("version"))
+	C4:AllowExecute(true)
+end
+
+------------------------------------------------------------------------------------------------
+--Function Name : OnDriverLateInit
+--Description   : Function that serves as a callback into a project after the project is loaded.
+------------------------------------------------------------------------------------------------
+function OnDriverLateInit()
+	for k,v in pairs(Properties) do OnPropertyChanged(k) end
+end
+
+-----------------------------------------------------------------------------------------------------------------------------
+--Function Name : OnDriverDestroyed
+--Description   : Function called when a driver is deleted from a project, updated within a project or Director is shut down.
+-----------------------------------------------------------------------------------------------------------------------------
+function OnDriverDestroyed()
+	gDbgPrint = C4:KillTimer(gDbgPrint or 0)
+end
+
+----------------------------------------------------------------------------
+--Function Name : OnPropertyChanged(strProperty)
+--Parameters    : strProperty(string)
+--Description   : Function called by Director when a property changes value.
+----------------------------------------------------------------------------
+function OnPropertyChanged (strProperty)
+	Dbg("OnPropertyChanged: " .. strProperty .. " (" .. Properties[strProperty] .. ")")
+	local propertyValue = Properties[strProperty]
+	if (propertyValue == nil) then propertyValue = '' end
+	local strProperty = string.upper(strProperty)
+	strProperty = string.gsub(strProperty, "%s+", "_")
+	local success, ret
+	if (OPC and OPC[strProperty] and type(OPC[strProperty]) == "function") then
+		success, ret = pcall(OPC[strProperty], propertyValue)
 	end
-	if (proxyId == "") then return end
-	for id in tList:gfind("(%d+),") do
-		local roomId = tonumber(id) or 0
-		if (roomId == 0) then break end
-		tParams["USERNAME"] = "primaryuser"
-		tParams["LOCATION_ID"] = roomId
-		tParams["NAME"] = "wallpaper"
-		tParams["VALUE"] = strWallpaper
-		C4:SendToDevice(proxyId, strCommand, tParams)
-		Dbg("C4:SendToDevice(" .. proxyId .. ", \"" .. strCommand .. "\", " .. formatParams(tParams) .. ")")
+	if (success == true) then
+		return (ret)
+	elseif (success == false) then
+		print ("OnPropertyChanged Lua error: ", strProperty, ret)
 	end
 end
 
---------------------------------------------------------------------------
---Function Name : GetWallpaperList
---Parameters    : currentValue(string)
---Description   : Function called with CUSTOM_SELECT Property in Commands.
---------------------------------------------------------------------------
-function GetWallpaperList(currentValue)
-	local list = {}
-	local wallpapers = {}
-	
-	-- Custom Wallpapers
-	local custom_dir = C4:FileSetDir("/media/wallpaper/onscreen/custom")
-	local custom_wallpapers = C4:FileList(custom_dir)
-
-	---- Add in custom wallpaper list from files on controller
-	for id, filename in pairs (custom_wallpapers) do
-		local item = {
-			value = "/media/wallpaper/onscreen/custom/" .. filename,
-			text = filename,
-		}
-		table.insert (list, item)
-	end
-
-	-- Default Wallpapers
-	local def_dir = C4:FileSetDir("/media/wallpaper/onscreen/default")
-	local def_wallpapers = C4:FileList(def_dir)
-
-	---- Add in wallpaper list from files on controller
-	for id, filename in pairs (def_wallpapers) do
-		local item = {
-			value = "/media/wallpaper/onscreen/default/" .. filename,
-			text = filename,
-		}
-		table.insert (list, item)
-	end
-
-	local _sort = function (a, b)
-		return (a.text < b.text)
-	end
-
-	table.sort (list, _sort)
-
-	return list
+-------------------------------------------------------------------------
+--Function Name : OPC.DEBUG_MODE(strProperty)
+--Parameters    : strProperty(string)
+--Description   : Function called when Debug Mode property changes value.
+-------------------------------------------------------------------------
+function OPC.DEBUG_MODE(strProperty)
+	gDbgPrint = C4:KillTimer(gDbgPrint or 0)
+	if (strProperty == "Off") then return end
+	gDbgPrint = C4:AddTimer(8, "HOURS")
+	print ("Enabled Debug Timer for 8 hours")
 end
 
 -----------------------------------------------------------------------------------------------------
@@ -118,38 +105,70 @@ function EC.SET_WALLPAPER(tParams)
 	SendToDevices(list, "SET_LOCATION_PREFERENCE", wallpaper)
 end
 
-----------------------------------------------------------------------------
---Function Name : OnPropertyChanged(strProperty)
---Parameters    : strProperty(string)
---Description   : Function called by Director when a property changes value.
-----------------------------------------------------------------------------
-function OnPropertyChanged (strProperty)
-	Dbg("OnPropertyChanged: " .. strProperty .. " (" .. Properties[strProperty] .. ")")
-	local propertyValue = Properties[strProperty]
-	if (propertyValue == nil) then propertyValue = '' end
-	local strProperty = string.upper(strProperty)
-	strProperty = string.gsub(strProperty, "%s+", "_")
-	local success, ret
-	if (OPC and OPC[strProperty] and type(OPC[strProperty]) == "function") then
-		success, ret = pcall(OPC[strProperty], propertyValue)
+-----------------------------------------------------------------------------
+--Function Name : SendToDevices
+--Parameters    : tList(table), strCommand(string), tParams(table)
+--Description   : Function called to send wallpaper change to Identity agent.
+-----------------------------------------------------------------------------
+function SendToDevices(tList, strCommand, strWallpaper)
+	tList = trim(tList) .. ","
+	local tParams = {}
+	local agent = "control4_agent_identity.c4i"
+	local devices = C4:GetDevicesByC4iName(agent)
+	local proxyId = ""
+	if (devices ~= nil) then
+		for k,v in pairs(devices) do
+			if (v == "Identity") then proxyId = k end
+		end
 	end
-	if (success == true) then
-		return (ret)
-	elseif (success == false) then
-		print ("OnPropertyChanged Lua error: ", strProperty, ret)
+	if (proxyId == "") then return end
+	for id in tList:gfind("(%d+),") do
+		local roomId = tonumber(id) or 0
+		if (roomId == 0) then break end
+		tParams["USERNAME"] = "primaryuser"
+		tParams["LOCATION_ID"] = roomId
+		tParams["NAME"] = "wallpaper"
+		tParams["VALUE"] = strWallpaper
+		C4:SendToDevice(proxyId, strCommand, tParams)
+		Dbg("C4:SendToDevice(" .. proxyId .. ", \"" .. strCommand .. "\", " .. formatParams(tParams) .. ")")
 	end
 end
 
--------------------------------------------------------------------------
---Function Name : OPC.DEBUG_MODE(strProperty)
---Parameters    : strProperty(string)
---Description   : Function called when Debug Mode property changes value.
--------------------------------------------------------------------------
-function OPC.DEBUG_MODE(strProperty)
-	gDbgPrint = C4:KillTimer(gDbgPrint or 0)
-	if (strProperty == "Off") then return end
-	gDbgPrint = C4:AddTimer(8, "HOURS")
-	print ("Enabled Debug Timer for 8 hours")
+--------------------------------------------------------------------------
+--Function Name : GetWallpaperList
+--Parameters    : currentValue(string)
+--Description   : Function called with CUSTOM_SELECT Property in Commands.
+--------------------------------------------------------------------------
+function GetWallpaperList(currentValue)
+	local list = {}
+	local wallpapers = {}
+	-- Custom Wallpapers
+	local custom_dir = C4:FileSetDir("/media/wallpaper/onscreen/custom")
+	local custom_wallpapers = C4:FileList(custom_dir)
+	---- Add in custom wallpaper list from files on controller
+	for id, filename in pairs (custom_wallpapers) do
+		local item = {
+			value = "/media/wallpaper/onscreen/custom/" .. filename,
+			text = filename,
+		}
+		table.insert (list, item)
+	end
+	-- Default Wallpapers
+	local def_dir = C4:FileSetDir("/media/wallpaper/onscreen/default")
+	local def_wallpapers = C4:FileList(def_dir)
+	---- Add in wallpaper list from files on controller
+	for id, filename in pairs (def_wallpapers) do
+		local item = {
+			value = "/media/wallpaper/onscreen/default/" .. filename,
+			text = filename,
+		}
+		table.insert (list, item)
+	end
+	local _sort = function (a, b)
+		return (a.text < b.text)
+	end
+	table.sort (list, _sort)
+	return list
 end
 
 ---------------------------------------------------------------------------------------------
@@ -187,33 +206,3 @@ function formatParams(tParams)
 	end
 	return "{" .. table.concat(out, ", ") .. "}"
 end
-
------------------------------------------------------------------------------------------------------------------------------
---Function Name : OnDriverDestroyed
---Description   : Function called when a driver is deleted from a project, updated within a project or Director is shut down.
------------------------------------------------------------------------------------------------------------------------------
-function OnDriverDestroyed()
-	gDbgPrint = C4:KillTimer(gDbgPrint or 0)
-end
-
-----------------------------------------------------------------------------
---Function Name : OnDriverInit
---Description   : Function invoked when a driver is loaded or being updated.
-----------------------------------------------------------------------------
-function OnDriverInit()
-	C4:AllowExecute(true)
-end
-
-------------------------------------------------------------------------------------------------
---Function Name : OnDriverLateInit
---Description   : Function that serves as a callback into a project after the project is loaded.
-------------------------------------------------------------------------------------------------
-function OnDriverLateInit()
-	DRIVER_NAME = C4:GetDriverConfigInfo("name")
-	DRIVER_VERSION = C4:GetDriverConfigInfo("version")
-	C4:UpdateProperty("Driver Name", DRIVER_NAME)
-	C4:UpdateProperty("Driver Version", DRIVER_VERSION)
-	for k,v in pairs(Properties) do OnPropertyChanged(k) end
-end
-
-print("Driver Loaded..." .. os.date())
